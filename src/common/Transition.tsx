6e8de24ac88e0e2849288801b3abe3abab76c959
deepesh-sr <rathoredeepeshsingh@gmail.com>
2025-09-27 14:57:26 +0530

    transition added

--- src/common/Transition.tsx
+++ src/common/Transition.tsx
@@ -1,21 +1,64 @@
+import { useEffect, useRef } from "react"
+import { useLocation } from "react-router-dom"
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+const Transition = ({ children }: { children: React.ReactNode }) => {
+
+    const divRef = useRef<HTMLDivElement>(null);
+    const childRef = useRef<HTMLDivElement>(null);
+    const location = useLocation();
+    useEffect(() => {
+        let tl = gsap.timeline();
+
+        tl.to(divRef.current, {
+            display: 'block flex'
+        })
+
+        tl.from('.stair', {
+            width: 0,
+            stagger: 0.15
+        })
+        tl.to('.stair', {
+            x: '100%',
+            stagger: 0.1
+        })
+        tl.to(divRef.current, {
+            display: 'none'
+        })
+        tl.to('.stair', {
+            x: '0%',
+        })
+
+        // const timer = setTimeout(()=>{
+        //   if (divRef.current) {
+        //     divRef.current.style.display = 'none';
+        //   }
+        //   return clearInterval(timer)
+        // },2000)
+        gsap.from(childRef.current, {
+            opacity: 0,
+            delay: 1.72
+        })
 
 
-const Transition = ({children} :{ children : React.ReactNode}) => {
-  return (
-    <div>
-        <div className=" h-screen w-full flex flex-col">
-            <div className="h-1/6 bg-red-900 "></div>
-            <div className="h-1/6 bg-blue-600 "></div>
-            <div className="h-1/6 bg-red-900 "></div>
-            <div className="h-1/6 bg-blue-600 "></div>
-            <div className="h-1/6 bg-red-900 "></div>
-            <div className="h-1/6 bg-blue-600 "></div>
-        </div>
+    }, [location.pathname])
+    return (
         <div>
-            {children}
+            <div ref={divRef} className=" h-screen w-full flex flex-col">
+                <div className="stair h-1/6 bg-red-900 "></div>
+                <div className="stair h-1/6 bg-blue-600 "></div>
+                <div className="stair h-1/6 bg-red-900 "></div>
+                <div className="stair h-1/6 bg-blue-600 "></div>
+                <div className="stair h-1/6 bg-red-900 "></div>
+                <div className="stair h-1/6 bg-blue-600 "></div>
+            </div>
+            <div ref={childRef}>
+                {children}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Transition
