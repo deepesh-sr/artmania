8df929ea2b2cfe1fac19510b65d12407f4a2dfbc
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-01-01 19:14:37 +0530

    Add : Portfolion and service

---
+++ src/pages/PortfolioPage.tsx
@@ -0,0 +1,13 @@
+import Portfolio from '../components/Portfolio'
+
+const PortfolioPage = () => {
+  return (
+    <div className='min-h-screen w-full bg-[#FAFBFC]'>
+      <div className="pt-20">
+        <Portfolio />
+      </div>
+    </div>
+  )
+}
+
+export default PortfolioPage
---
+++ src/pages/ServicesPage.tsx
@@ -0,0 +1,13 @@
+import Services from '../components/Services'
+
+const ServicesPage = () => {
+  return (
+    <div className='min-h-screen w-full bg-[#FAFBFC]'>
+      <div className="pt-20">
+        <Services />
+      </div>
+    </div>
+  )
+}
+
+export default ServicesPage
